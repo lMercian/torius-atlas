a7d18eb2f442cca6914d1c45f263c002409f508c
lMercian <70115720+lMercian@users.noreply.github.com>
2026-03-27 13:51:35 -0500

    fix vite syntax

--- server/_core/vite.ts
+++ server/_core/vite.ts
@@ -24,43 +24,22 @@ export async function setupVite(app: Express, server: Server) {
   app.use("*", async (req, res, next) => {
     const url = req.originalUrl;
 
-    try {
-    const clientTemplate =
-  process.env.NODE_ENV === "production"
-    ? path.resolve("dist", "public", "index.html")
-    : path.resolve("client", "index.html");
-      
+   try {
+  const clientTemplate =
+    process.env.NODE_ENV === "production"
+      ? path.resolve("dist", "public", "index.html")
+      : path.resolve("client", "index.html");
 
-      // always reload the index.html file from disk incase it changes
-      let template = await fs.promises.readFile(clientTemplate, "utf-8");
-      template = template.replace(
-        `src="/src/main.tsx"`,
-        `src="/src/main.tsx?v=${nanoid()}"`
-      );
-      const page = await vite.transformIndexHtml(url, template);
-      res.status(200).set({ "Content-Type": "text/html" }).end(page);
-    } catch (e) {
-      vite.ssrFixStacktrace(e as Error);
-      next(e);
-    }
-  });
-}
+  // always reload the index.html file from disk incase it changes
+  let template = await fs.promises.readFile(clientTemplate, "utf-8");
+  template = template.replace(
+    `src="/src/main.tsx"`,
+    `src="/src/main.tsx?v=${nanoid()}"`
+  );
 
-export function serveStatic(app: Express) {
-  const distPath =
-    process.env.NODE_ENV === "development"
-      ? path.resolve(import.meta.dirname, "../..", "dist", "public")
-      : path.resolve(import.meta.dirname, "public");
-  if (!fs.existsSync(distPath)) {
-    console.error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`
-    );
-  }
-
-  app.use(express.static(distPath));
-
-  // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
-  });
+  const page = await vite.transformIndexHtml(url, template);
+  res.status(200).set({ "Content-Type": "text/html" }).end(page);
+} catch (e) {
+  vite.ssrFixStacktrace(e as Error);
+  next(e);
 }
