36a6d35e651232d556d2aed482b5b56f2305d42d
lMercian <70115720+lMercian@users.noreply.github.com>
2026-03-27 14:50:44 -0500

    fix vite syntax

--- server/_core/vite.ts
+++ server/_core/vite.ts
@@ -24,21 +24,25 @@ export async function setupVite(app: Express, server: Server) {
   app.use("*", async (req, res, next) => {
     const url = req.originalUrl;
 
-   try {
+  try {
   const clientTemplate =
     process.env.NODE_ENV === "production"
       ? path.resolve("dist", "public", "index.html")
       : path.resolve("client", "index.html");
 
-  // always reload the index.html file from disk incase it changes
   let template = await fs.promises.readFile(clientTemplate, "utf-8");
+
   template = template.replace(
-    `src="/src/main.tsx"`,
+    'src="/src/main.tsx"',
     `src="/src/main.tsx?v=${nanoid()}"`
   );
 
   const page = await vite.transformIndexHtml(url, template);
-  res.status(200).set({ "Content-Type": "text/html" }).end(page);
+
+  res.status(200)
+    .set({ "Content-Type": "text/html" })
+    .end(page);
+
 } catch (e) {
   vite.ssrFixStacktrace(e as Error);
   next(e);
